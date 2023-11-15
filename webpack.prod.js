1d51a4feaf54c5f96c6532f440fc9565efae6462
Michael Ruocco <michael.ruocco@digital.homeoffice.gov.uk>
2023-11-15 14:59:41 +0000

    removes dundent config from prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,8 +8,7 @@ module.exports = merge(common, {
   plugins: [
     new webpack.DefinePlugin({
       APP_API_BASE_URL: JSON.stringify('/api'),
-      APP_WEB_SOCKET_BASE_URL: JSON.stringify('/web-socket'),
-      APP_WEB_SOCKET_PREFIX_WINDOW_ORIGIN: JSON.stringify(false),
+      APP_WEB_SOCKET_BASE_URL: JSON.stringify('/web-socket')
     }),
   ],
 });
